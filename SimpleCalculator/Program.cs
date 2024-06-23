925664fd2ebbaf3580a8ca314f9461bbeb8a71b2
wuuw4 <105397457+wuuw4@users.noreply.github.com>
2024-06-23 19:37:27 +0400

    Add files via upload

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("Деление на ноль невозможно", nameof(b));
+            }
+            return a / b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            Console.WriteLine($"5 + 3 = {calc.Add(5, 3)}");
+            Console.WriteLine($"5 - 3 = {calc.Subtract(5, 3)}");
+            Console.WriteLine($"5 * 3 = {calc.Multiply(5, 3)}");
+            Console.WriteLine($"5 / 3 = {calc.Divide(5, 3)}");
+
+            Console.ReadLine();
+        }
+    }
+}
